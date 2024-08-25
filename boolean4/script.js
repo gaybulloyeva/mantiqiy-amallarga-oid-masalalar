59fd95179f9226bdc839a71d1428fa2c249e2ff0
new <gaybullayevamrayhona718@gmail.com>
2024-08-25 17:58:16 +0500

    boolean4 git commit -m boolean4
    git commit -m boolean4

---
+++ boolean4/script.js
@@ -0,0 +1,14 @@
+function solve() {
+    let A = prompt("butun sonni kiriting")
+    let B = prompt("butun sonni kiriting")
+
+
+
+    if (A > 2 && B <= 3) {
+        document.getElementById("demo").innerHTML = "True"
+    }
+
+    else {
+        document.getElementById("demo").innerHTML = "False"
+    }
+}
