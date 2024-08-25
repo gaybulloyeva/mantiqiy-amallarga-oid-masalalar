cb2df79868518058c1553a8d6f03e1a5af98e1cf
new <gaybullayevamrayhona718@gmail.com>
2024-08-25 18:21:12 +0500

    boolean30

---
+++ boolean30/script.js
@@ -0,0 +1,19 @@
+function solve() {
+    let a = prompt("a ga qiymat kiriting")
+    let b = prompt("b ga qiymat kiriting")
+    let c = prompt("c ga qiymat kiriting")
+
+    if (a != 0 && b != 0 && c != 0 && a > 0 && b > 0 && c > 0) {
+        if (a == b && b == c && c == a) {
+            document.getElementById("demo").innerHTML = a + ", " + b + ", " + c + "tomonli uchburchak teng tomonli uchburchak"
+        }
+
+        else {
+            document.getElementById("demo").innerHTML = a + ", " + b + ", " + c + "tomonli uchburchak teng tomonli uchburchak emas"
+        }
+    }
+
+    else {
+        document.getElementById("demo").innerHTML = "uchburchak yasab bo'lmaydi"
+    }
+}
