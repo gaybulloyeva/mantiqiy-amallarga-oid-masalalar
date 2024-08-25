01cb7f8318588e48e887dab06c6a76f8cfa30d01
new <gaybullayevamrayhona718@gmail.com>
2024-08-25 18:17:33 +0500

    boolean26

---
+++ boolean26/script.js
@@ -0,0 +1,16 @@
+function solve() {
+    let x = prompt("x ga qiymat kiriting")
+    let y = prompt("y ga qiymat kiriting")
+
+    if (x > 0 && y < 0) {
+        document.getElementById("demo").innerHTML = "bu nuqta 4-chorakda yotadi"
+    }
+
+    else if (x == 0 && y == 0) {
+        document.getElementById("demo").innerHTML = "bu nuqta koordinatalar boshida yotadi"
+    }
+
+    else {
+        document.getElementById("demo").innerHTML = "bu nuqta 4-chorakda yotmaydi"
+    }
+}
