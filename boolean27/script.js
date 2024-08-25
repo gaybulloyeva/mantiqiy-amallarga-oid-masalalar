73d636447bb6187de8f5a56c03eb2d9f4aafd8b5
new <gaybullayevamrayhona718@gmail.com>
2024-08-25 18:18:44 +0500

    boolean27

---
+++ boolean27/script.js
@@ -0,0 +1,21 @@
+function solve() {
+    let x = prompt("x ga qiymat kiriting")
+    let y = prompt("y ga qiymat kiriting")
+
+    if (x < 0 && y > 0 || x < 0 && y < 0) {
+        document.getElementById("demo").innerHTML = "bu nuqta 2 yoki 3-chorakda yotadi"
+    }
+
+
+
+
+
+    else if (x == 0 && y == 0) {
+        document.getElementById("demo").innerHTML = "bu nuqta koordinatalar boshida yotadi"
+    }
+
+    else {
+        document.getElementById("demo").innerHTML = "bu nuqta 2-chorakda ham 3-chorakda ham yotmaydi"
+    }
+
+}
